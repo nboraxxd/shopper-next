24e39c5ad39834c7695954a85992236b6994ddd6
Bora <nhatbaoxxd@gmail.com>
2025-04-24 00:06:41 +0700

    Revert "feat: Reponsive order detail page (WIP)"
    This reverts commit 95d77185b6c04ad5f11bd049fbc01fb0c63ec8ca.

--- src/features/order/components/server/order-detail-content.tsx
+++ src/features/order/components/server/order-detail-content.tsx
@@ -1,16 +1,16 @@
 import orderServerApi from '@/features/order/api/server'
-import { ORDER_ITEM_STATUS, ORDER_STATUS } from '@/features/order/constants'
+import { ORDER_STATUS } from '@/features/order/constants'
 import { OrderDetailResponse, OrderStatus } from '@/features/order/types'
-import { cn, formatCurrency } from '@/shared/utils'
+import { cn } from '@/shared/utils'
 import { format } from 'date-fns'
 import { CircleXIcon, PackageCheckIcon, PackageSearchIcon, ReceiptTextIcon, TruckIcon } from 'lucide-react'
 import Image from 'next/image'
 
 const ACTIVE_ORDER_STEP_STYLES =
-  'data-[slot=circle]:*:border-highlight data-[slot=circle]:*:bg-highlight [&_[data-slot=label]]:*:text-foreground [&_[data-slot=icon]]:text-light-1 [&_[data-slot=icon]]:dark:text-secondary-1'
+  'data-[slot=circle]:*:border-highlight data-[slot=circle]:*:bg-highlight data-[slot=label]:*:text-foreground [&_[data-slot=icon]]:text-light-1 [&_[data-slot=icon]]:dark:text-secondary-1'
 
 const COMPLETED_ORDER_STEP_STYLES =
-  'data-[slot=circle]:*:border-highlight [&_[data-slot=label]]:*:text-foreground [&_[data-slot=icon]]:text-highlight'
+  'data-[slot=circle]:*:border-highlight data-[slot=label]:*:text-foreground [&_[data-slot=icon]]:text-highlight'
 
 export const ORDER_STEP_STATUS = {
   [ORDER_STATUS.PENDING]: {
@@ -67,32 +67,9 @@ export default async function OrderDetailContent({ id, accessToken }: { id: stri
   const stepNumber = Object.keys(ORDER_STEP_STATUS).findIndex((key) => key === orderDetail.status)
 
   return (
-    <div className="mt-3 md:mt-5">
-      <div className="rounded-lg border bg-secondary-5 p-3 dark:bg-dark-2 xs:p-4 md:px-16">
-        <div className="flex flex-col justify-between gap-1.5 xs:flex-row">
-          <p className="flex-col items-center gap-2 text-xs xs:flex">
-            <span className="font-semibold uppercase text-secondary-2">
-              Mã đơn hàng<span className="xs:hidden">: </span>
-            </span>
-            <span className="text-sm uppercase">{orderDetail._id.slice(0, 12)}</span>
-          </p>
-          <p className="flex-col items-center gap-2 text-xs xs:flex">
-            <span className="font-semibold uppercase text-secondary-2">
-              Trạng thái<span className="xs:hidden">: </span>
-            </span>
-            <span className="text-sm">{ORDER_ITEM_STATUS[orderDetail.status]['label']}</span>
-          </p>
-          <p className="flex-col items-center gap-2 text-xs xs:flex">
-            <span className="font-semibold uppercase text-secondary-2">
-              Tổng tiền<span className="xs:hidden">: </span>
-            </span>
-            <span className="text-sm text-highlight">
-              {formatCurrency(orderDetail.total)}
-              <sup>₫</sup>
-            </span>
-          </p>
-        </div>
-      </div>
+    <div className="mt-1 md:mt-3">
+      <p className="text-end text-sm uppercase">Mã đơn hàng: {orderDetail._id.slice(0, 12)}</p>
+
       {orderDetail.status === 'cancel' ? (
         <div className="relative mt-3 grid grid-cols-2 md:mt-5">
           <OrderStep
@@ -104,7 +81,7 @@ export default async function OrderDetailContent({ id, accessToken }: { id: stri
           <div className="absolute left-1/2 top-[1.375rem] h-1 w-1/2 -translate-x-1/2 bg-highlight" />
         </div>
       ) : (
-        <div className="relative mx-auto mt-3 grid w-fit grid-rows-4 xs:mx-0 xs:w-full xs:grid-cols-4 xs:grid-rows-none md:mt-5">
+        <div className="relative mt-3 grid grid-cols-4 md:mt-5">
           {ORDER_STEPS.map((step, index) => (
             <OrderStep
               key={step.status}
@@ -112,22 +89,15 @@ export default async function OrderDetailContent({ id, accessToken }: { id: stri
               date={orderDetail[step.dateKey]}
               className={cn(
                 stepNumber === index && ACTIVE_ORDER_STEP_STYLES,
-                stepNumber > index && COMPLETED_ORDER_STEP_STYLES,
-                'order-[var(--step-number)] xs:order-none'
+                stepNumber > index && COMPLETED_ORDER_STEP_STYLES
               )}
-              style={{ '--step-number': ORDER_STEPS.length - index } as React.CSSProperties}
             />
           ))}
-          <div className="absolute top-[1.125rem] ml-[calc(100%/8)] hidden h-1 w-[calc(100%-(100%/4))] bg-secondary-2 xs:block md:top-[1.375rem]" />
+          <div className="absolute top-[1.375rem] ml-[calc(100%/8)] h-1 w-[calc(100%-(100%/4))] bg-secondary-2" />
           <div
             className={cn(
-              'absolute top-[1.125rem] ml-[calc(100%/8)] hidden h-1 w-[calc(100%/4*var(--step-number))] bg-highlight xs:block md:top-[1.375rem]'
+              'absolute top-[1.375rem] ml-[calc(100%/8)] h-1 w-[calc(100%/4*var(--step-number))] bg-highlight'
             )}
-            style={{ '--step-number': stepNumber } as React.CSSProperties}
-          />
-          <div className="absolute bottom-[calc(100%/8)] left-[14px] h-[calc(100%-(100%/4))] w-1 bg-secondary-2 xs:hidden" />
-          <div
-            className="absolute bottom-[calc(100%/8)] left-[14px] h-[calc(100%/4*var(--step-number))] w-1 bg-highlight xs:hidden"
             style={{ '--step-number': stepNumber } as React.CSSProperties}
           />
         </div>
@@ -140,30 +110,23 @@ interface OrderStepProps {
   status: OrderStatus
   date?: number
   className?: string
-  style?: React.CSSProperties
 }
 
-function OrderStep({ status, date, className, style }: OrderStepProps) {
+function OrderStep({ status, date, className }: OrderStepProps) {
   const { icon: Icon, label } = ORDER_STEP_STATUS[status]
 
   return (
-    <div className={cn('z-10 flex min-h-10 items-center gap-2 xs:min-h-max xs:flex-col', className)} style={style}>
+    <div className={cn('z-10 flex flex-col items-center gap-1', className)}>
       <div
-        className="flex size-8 items-center justify-center rounded-full border-[3px] border-secondary-2 bg-account-section xs:size-10 md:size-12"
+        className="flex size-12 items-center justify-center rounded-full border-[3px] border-secondary-2 bg-account-section"
         data-slot="circle"
       >
-        <Icon className="size-4 text-secondary-2 xs:size-5 md:size-6" data-slot="icon" />
+        <Icon className="text-secondary-2" data-slot="icon" />
       </div>
-      <div className="flex flex-col xs:items-center">
-        <p className="text-xs text-secondary-2 md:text-sm" data-slot="label">
-          {label}
-        </p>
-        {date ? (
-          <p className="mt-0.5 text-[0.625rem] text-secondary-2 md:text-xs">
-            {format(new Date(date), 'HH:mm dd/MM/yyyy')}
-          </p>
-        ) : null}
-      </div>
+      <p className="mt-1 text-sm text-secondary-2" data-slot="label">
+        {label}
+      </p>
+      {date ? <p className="text-xs text-secondary-2">{format(new Date(date), 'HH:mm dd/MM/yyyy')}</p> : null}
     </div>
   )
 }
